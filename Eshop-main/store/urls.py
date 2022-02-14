210027fc63593c4b3c6eab2654415d7cd5d29e7f
reanbrenda <mukindiabrenda67@gmail.com>
2022-02-14 06:58:59 +0300

    helo

--- Eshop-main/store/urls.py
+++ Eshop-main/store/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views.home import Index , store
+from .views.home import Index , store, home
 from .views.signup import Signup
 from .views.login import Login , logout
 from .views.cart import Cart
@@ -11,7 +11,9 @@ from .middlewares.auth import  auth_middleware
 
 
 urlpatterns = [
-    path('', Index.as_view(), name='homepage'),
+    
+    path('homepage', Index.as_view(), name='homepage'),
+    path('', home, name='home'),
     path('store', store , name='store'),
     path('signup', Signup.as_view(), name='signup'),
     path('login', Login.as_view(), name='login'),
